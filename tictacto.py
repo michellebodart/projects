fd0939d8613698297e6e8aa9cd080b6914b84fbb
michellebodart <71563911+michellebodart@users.noreply.github.com>
2020-12-30 13:32:30 -0800

    Update tictacto.py

--- tictacto.py
+++ tictacto.py
@@ -1,4 +1,5 @@
 import random
+import re
 
 def printboard(list):
   print()
@@ -12,7 +13,6 @@ def printboard(list):
 
 def rowlinediag(list):
   return [list[0],list[1],list[2],[list[0][0],list[1][0],list[2][0]],[list[0][1],list[1][1],list[2][1]],[list[0][2],list[1][2],list[2][2]],[list[0][0],list[1][1],list[2][2]],[list[2][0],list[1][1],list[0][2]]]
-
 
 def tictacto():
   print("Welcome to Tic Tac To\nPlease enter your names")
@@ -38,8 +38,15 @@ def tictacto():
   i = 0
 
   while winner == "" and i<9:
+
+    playerinput = input(playerlist[i%2][0]+", choose an open position (x,y): ")
+    correct_syntax = '^ *[123] *, *[123] *$'
+    is_correct_syntax = re.match(correct_syntax,playerinput)
+    while is_correct_syntax == None:
+      playerinput = input(playerlist[i%2][0]+", please use proper syntax and choose a position within the range (x,y): ")
+      is_correct_syntax = re.match(correct_syntax,playerinput)
     
-    x, y = input(playerlist[i%2][0]+", choose an open position (x,y): ").split(",")
+    x, y = playerinput.split(",")
     if gameboard[int(y)-1][int(x)-1] == " ":
       gameboard[int(y)-1][int(x)-1] = playerlist[i%2][1]
       printboard(gameboard)
@@ -47,7 +54,13 @@ def tictacto():
     else:
       emptyspot = False
       while emptyspot == False:
-        x, y = input(playerlist[i%2][0]+", that spot is taken, please choose another: ").split(",")
+        playerinput = input(playerlist[i%2][0]+", that spot is taken, please choose another: ")
+        is_correct_syntax = re.match(correct_syntax,playerinput)
+        while is_correct_syntax == None:
+          playerinput = input(playerlist[i%2][0]+", please use proper syntax and choose an open position within the range (x,y): ")
+          is_correct_syntax = re.match(correct_syntax,playerinput)
+
+        x, y = playerinput.split(",")
         if gameboard[int(y)-1][int(x)-1] == " ":
           emptyspot = True
           gameboard[int(y)-1][int(x)-1] = playerlist[i%2][1]
@@ -65,5 +78,3 @@ def tictacto():
     print("Tie game, no one wins :/")
   else:
     print(winner+" wins!!!")
-
-
